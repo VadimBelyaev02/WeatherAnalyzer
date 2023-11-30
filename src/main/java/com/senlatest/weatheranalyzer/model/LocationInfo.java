7f0eaf3e2550d8d5c222f76bf582353f80d84eb1
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-11-30 21:54:15 +0300

    Created class to store info about location

---
+++ src/main/java/com/senlatest/weatheranalyzer/model/LocationInfo.java
@@ -0,0 +1,37 @@
+package com.senlatest.weatheranalyzer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+@Builder
+@NoArgsConstructor
+public class LocationInfo {
+    private String place_id;
+    private String licence;
+    private String osm_type;
+    private long osm_id;
+    private double lat;
+    private double lon;
+    private String classType;
+    private String type;
+    private int place_rank;
+    private double importance;
+    private String addresstype;
+    private String name;
+    private String display_name;
+    private List<String> boundingbox;
+
+    public Coordinates getLatAndLon() {
+        return Coordinates.builder()
+                .latitude(lat)
+                .longitude(lon)
+                .build();
+    }
+}
+
