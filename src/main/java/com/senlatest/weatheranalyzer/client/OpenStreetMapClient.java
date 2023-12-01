8cdab490deeab4a8784829cf31bccbdadfc87756
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-12-01 17:54:11 +0300

    Created  a class to send requests to open street map

--- src/main/java/com/senlatest/weatheranalyzer/client/OpenStreetMapClient.java
+++ src/main/java/com/senlatest/weatheranalyzer/client/OpenStreetMapClient.java
@@ -1,11 +1,13 @@
 package com.senlatest.weatheranalyzer.client;
 
-import com.senlatest.weatheranalyzer.model.Coordinates;
-import com.senlatest.weatheranalyzer.model.LocationInfo;
+import com.senlatest.weatheranalyzer.client.model.Coordinates;
+import com.senlatest.weatheranalyzer.client.model.LocationInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -14,7 +16,6 @@ public class OpenStreetMapClient {
     private final String BASE_URI = "https://nominatim.openstreetmap.org/search";
     private final WebClient webClient;
 
-
     public Coordinates getLonAndLatByCityName(String cityName) {
         String uri = UriComponentsBuilder.fromUriString(BASE_URI)
                 .queryParam("q", cityName)
@@ -22,11 +23,11 @@ public class OpenStreetMapClient {
                 .build()
                 .toUriString();
 
-        return webClient.get()
-                .uri(uri)
-                .retrieve()
-                .bodyToMono(LocationInfo.class)
-                .block()
+        return Objects.requireNonNull(webClient.get()
+                        .uri(uri)
+                        .retrieve()
+                        .bodyToMono(LocationInfo.class)
+                        .block())
                 .getLatAndLon();
 
     }
